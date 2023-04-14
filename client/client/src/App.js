708ec9a9b192bbee3727e75cb67ca25ef21fe6b1
chandrashekharjoshi302 <chandrashekharjoshi02@gmail.com>
2023-04-14 12:36:28 +0530

    Navbar page working

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,12 +1,36 @@
+import { Box, Flex, Spacer, Link, Button } from "@chakra-ui/react";
+import { BrowserRouter as Router, Switch, Route, Link as RouterLink } from "react-router-dom";
+import Navbar from './Componets/Navbar/Navbar'
+import Wishlist from './Componets/SingleProducts/Wishlist';
+import Login from './Componets/Login/Login';
+import Register from './Componets/Registration/Register';
+import ProductPage from "./Componets/Products/ProductPage";
 
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      <Navbar />
+      <Box px={4}>
+        <Switch>
+          {/* <Route path="/" exact>
+            <h1>Home page</h1>
+          </Route> */}
+          <Route path="/wishlist">
+            <Wishlist />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/product/:id">
+            <ProductPage />
+          </Route>
+        </Switch>
+      </Box>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ client/client/src/Componets/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import { Box, Flex, Spacer, Link, Button } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+
+const Navbar = () => {
+    const styles = {
+       
+        
+      };
+  return (
+    <Box bg="gray.100" px={4}>
+      <Flex alignItems="center" justifyContent="space-between" h={16}>
+        <Link as={RouterLink} to="/">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRuAnyzHjbpD5C-_av_VpGGjkz2bq-x1TJm-YeHLP7FE_P5x-V87_hXLgj1UF1XpseRzo&usqp=CAU" alt="Logo" style={{ width: '200px', height: '100px' }}/>
+        </Link>
+        <Spacer />
+        <Box>
+          <Link as={RouterLink} to="/wishlist" mx={4}>
+            Wishlist
+          </Link>
+          <Link as={RouterLink} to="/login" mx={4}>
+            Login
+          </Link>
+          <Link as={RouterLink} to="/register" mx={4}>
+            Register
+          </Link>
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Navbar;
